40385fd8d191765c27cd7039a9ce0e37bc914874
alex imbastari <aimbastari@gmail.com>
2016-05-02 21:30:52 -0700

    first commit

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -11,4 +11,20 @@ describe('App' , () => {
   it('renders something', () => {
     expect(component).to.exist;
   });
+
+  it('shows the correct text', () => {
+    expect(component).to.contain('Comment Box');
+
+  });
+
+  it('shows a comment box', () => {
+    expect(component.find('.comment-box')).to.exist;
+
+  });
+
+  it('shows a comment list', () => {
+    expect(component.find('.comment-list')).to.exist;
+
+  });
+
 });
